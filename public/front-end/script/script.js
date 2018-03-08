942901ba56fd3b7342ccfadba2120f6f0d0d18d7
dangminhdat <dangminhdat.qnam@gmail.com>
2018-03-08 23:38:35 +0700

    add wow.js & animate.css

--- public/front-end/script/script.js
+++ public/front-end/script/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
 	$('a[href*="#"]:not([href="#my-app"])').on("click",function(){
+		$('li.nav-item a').each(function(index,value){
+			if($('li.nav-item a').eq(index).hasClass('color'))
+				$('li.nav-item a').eq(index).removeClass("color");
+		});
+		$(this).addClass("color");
 		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 	 		var target = $(this.hash);
 		 	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -11,5 +16,13 @@ $(document).ready(function(){
 			}
 		}
 	});
+
+	// $('a[href*="#"]:not([href="#my-app"])').on("click",function(){
+	// 	e.preventDefault();
+	// 	id = $(this).attr('href');
+	// 	position = $(id).position().top;
+	// 	$('html,body').animate({scrollTop: pos},500);
+	// 	return false;
+	// });
 	
 });
